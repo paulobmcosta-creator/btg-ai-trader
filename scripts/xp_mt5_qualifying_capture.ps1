7bdd3e6bd70782bf8b5cf3ab8aa62e369b593f7d
paulobmcosta-creator <paulobmcosta@gmail.com>
2026-09-15 16:05:30 -0300

    S1: add passive MT5 capture launcher skeleton

---
+++ scripts/xp_mt5_qualifying_capture.ps1
@@ -0,0 +1,9 @@
+param(
+    [string]$Instrument = "WINV26"
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+Write-Host "BTG Sprint 1 passive capture launcher"
+Write-Host "Instrument: $Instrument"
